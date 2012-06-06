4d1d0f36d8336f8fad2bf791b1c190c11b0022d9
Aaron Leung <aaron.leung@moovweb.com>
2012-06-06 16:27:56 -0700

    Adding our build-thingies to our own fork.

---
+++ jenkins.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+[ -e $HOME/gobuilds/tools/jenkins-env.sh ] && source $HOME/gobuilds/tools/jenkins-env.sh
+[ -z $CLIBS_HOME ] && export CLIBS_HOME=$HOME/moovweb/clibs
+[ ! -d $CLIBS_HOME ] && mkdir -p $CLIBS_HOME
+
+make install PREFIX=$CLIBS_HOME
