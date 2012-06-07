520e3814d5f5f29ae5e3c59085a2c24da684ac9c
Aaron Leung <aaron.leung@moovweb.com>
2012-06-06 18:30:10 -0700

    Changing the build script so it'll actually work.

--- jenkins.sh
+++ jenkins.sh
@@ -3,6 +3,7 @@
 [ -z $CLIBS_HOME ] && export CLIBS_HOME=$HOME/moovweb/clibs
 [ ! -d $CLIBS_HOME ] && mkdir -p $CLIBS_HOME
 
-git pull upstream master
+make clean
+git pull git://github.com/hcatlin/libsass.git
 git push origin master
 make install PREFIX=$CLIBS_HOME
