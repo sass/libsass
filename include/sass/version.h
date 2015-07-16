4f746b9c851dcda470fe3e709254b8d6a7c8a0ab
Marcel Greter <marcel.greter@ocbnet.ch>
2015-07-16 20:59:48 +0200

    [WIP] Add initial cmake build files
    Still lots of details to work out, but basic building seems
    to work well. Also need to sort the mess with the includes.

---
+++ include/sass/version.h
@@ -0,0 +1,18 @@
+// Hampton Catlin. Copyright (C) 2012. MIT
+
+#ifndef HEADER_SASS_VERSION_H_INCLUDED
+#define HEADER_SASS_VERSION_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int sass_version_major(void);
+int sass_version_minor(void);
+int sass_version_patch(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
