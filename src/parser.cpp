f18c8dbab93841b5608b9c13f805cb19b47c03f2
xzyfer <xzyfer@gmail.com>
2017-05-02 12:44:18 +0200

    Fix segault when while predicate is missing
    Originally reported by @mrtuxracer

--- src/parser.cpp
+++ src/parser.cpp
@@ -2237,6 +2237,9 @@ namespace Sass {
   Supports_Block_Obj Parser::parse_supports_directive()
   {
     Supports_Condition_Obj cond = parse_supports_condition();
+    if (!cond) {
+      css_error("Invalid CSS", " after ", ": expected @supports condition (e.g. (display: flexbox)), was ", false);
+    }
     // create the ast node object for the support queries
     Supports_Block_Obj query = SASS_MEMORY_NEW(Supports_Block, pstate, cond);
     // additional block is mandatory
