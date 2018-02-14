fb241b3f0994043609c941fba7bcecd1ec8ce624
Marcel Greter <marcel.greter@ocbnet.ch>
2018-02-14 20:52:05 +0100

    Fix css test for interpolated numbers

--- src/eval.cpp
+++ src/eval.cpp
@@ -1151,7 +1151,9 @@ namespace Sass {
       ex = ll;
     }
     if (Number_Ptr nr = Cast<Number>(ex)) {
-      if (!nr->is_valid_css_unit()) {
+      Number reduced(nr);
+      reduced.reduce();
+      if (!reduced.is_valid_css_unit()) {
         throw Exception::InvalidValue(*nr);
       }
     }
