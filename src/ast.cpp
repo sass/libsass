sass/node-sass

commit 4254054dec66bb715d3a16ce3a9cf40501522773

Fix explosion of parent selectors in selector-nest

src/ast.cpp
@@ -1263,12 +1263,8 @@ namespace Sass {
   {
     if (!this->has_parent_ref()) return this;
     Selector_List_Ptr ss = SASS_MEMORY_NEW(Selector_List, pstate());
-    Selector_List_Ptr ps = pstack.back();
-    for (size_t pi = 0, pL = ps->length(); pi < pL; ++pi) {
-      for (size_t si = 0, sL = this->length(); si < sL; ++si) {
-        Selector_List_Obj rv = at(si)->resolve_parent_refs(pstack, traces, implicit_parent);
-        ss->concat(rv);
-      }
+    for (size_t si = 0, sL = this->length(); si < sL; ++si) {
+      ss->concat(at(si)->resolve_parent_refs(pstack, traces, implicit_parent));
     }
     return ss;
   }

sass/libsass

commit 03ab922b52b2d1e3fa4181d395e5478a2776e55d

Fix memory leak in resolve_parent_refs
Fixes a memory leak introduced in
https://github.com/sass/libsass/commit/4254054dec66bb715d3a16ce3a9cf40501522773.

I do not understand why it leaks without this change but it does seem
like a bug in `SharedPtr`.

Fixes #2746

src/ast.cpp
@@ -1243,7 +1243,8 @@ namespace Sass {
     if (!this->has_parent_ref()) return this;
     Selector_List_Ptr ss = SASS_MEMORY_NEW(Selector_List, pstate());
     for (size_t si = 0, sL = this->length(); si < sL; ++si) {
-      ss->concat(at(si)->resolve_parent_refs(pstack, traces, implicit_parent));
+      Selector_List_Obj rv = at(si)->resolve_parent_refs(pstack, traces, implicit_parent);
+      ss->concat(rv);
     }
     return ss;
   }